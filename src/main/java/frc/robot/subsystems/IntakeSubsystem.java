7acb3f6c5900d7cb5e9ee942bf7352479805d9a0
Vignesh Balasubramaniam <vigneshb.hp@gmail.com>
2022-01-26 00:46:27 -0500

    Cleanup IntakeSubsystem

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -171,14 +171,15 @@ public class IntakeSubsystem extends SubsystemBase implements AutoCloseable {
   }
 
  /**
-   * Stops motor only
+   * Stop roller
    */
   public void stop() {
     m_rollerMotor.stopMotor();
   }
 
   /**
-   * Stops motor and returns arm to previous position
+   * Stop roller and return arm to previous position
+   * @param prevArmPosition Arm position to return to
    */
   public void stop(ArmPosition prevArmPosition) {
     m_rollerMotor.stopMotor();
