7d59599f471c4c004a3f3f7e79c1a03e1f2d3837
Vignesh Balasubramaniam <vigneshb.hp@gmail.com>
2022-01-28 17:28:58 -0500

    Supress unused variable warnings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -83,6 +83,7 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
+  @SuppressWarnings("unused")
   private void configureButtonBindings() {
     JoystickButton primaryButtonA = new JoystickButton(PRIMARY_CONTROLLER, Button.kA.value);
     JoystickButton primaryButtonB = new JoystickButton(PRIMARY_CONTROLLER, Button.kB.value);
