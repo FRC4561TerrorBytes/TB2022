d8c8cbc5ff688c3e9414aff0e38d0c686a25f863
Vignesh Balasubramaniam <vigneshb.hp@gmail.com>
2022-02-22 04:44:56 -0500

    Code cleanup

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -75,7 +75,7 @@ public class RobotContainer {
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
-    configureButtonBindings();
+    configureButtonBindings(); 
 
      // Initialize Automode Chooser in Shuffleboard
      autoModeChooser();
