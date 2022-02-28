c8c186f0655212792184c08ea004dd5f02b0ed7a
Vignesh Balasubramaniam <vigneshb.hp@gmail.com>
2022-02-27 23:02:14 -0500

    Set intake motors to brake mode

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -6,8 +6,10 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.MathUtil;
@@ -69,9 +71,11 @@ public class IntakeSubsystem extends SubsystemBase implements AutoCloseable {
     m_armPosition = ArmPosition.Top;
     m_prevArmPosition = m_armPosition;
     
+    m_rollerMotor.setIdleMode(IdleMode.kBrake);
     m_rollerMotor.setInverted(false);
 
     m_armConfig.initializeTalonPID(m_armMotor, FeedbackDevice.IntegratedSensor);
+    m_armMotor.setNeutralMode(NeutralMode.Brake);
     m_armMotor.setSelectedSensorPosition(0.0);
   }
 
