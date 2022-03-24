f8e5855f9117ce8523997c102400d459202701c2
afredge <afrederick23@rthighschool.org>
2022-03-23 20:31:51 -0400

    Thanks connor

--- src/main/java/frc/robot/commands/autonomous/FiveBallAuto.java
+++ src/main/java/frc/robot/commands/autonomous/FiveBallAuto.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.autonomous;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.commands.IntakeCommand;
 import frc.robot.commands.ShootCommand;
@@ -9,22 +10,35 @@ import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.ShooterSubsystem.SelectedGoal;
 import frc.robot.utils.AutoTrajectory;
 
-public class FiveBallAuto extends ThreeBallAuto {
+public class FiveBallAuto extends SequentialCommandGroup {
 
   public FiveBallAuto(DriveSubsystem driveSubsystem, IntakeSubsystem intakeSubsystem, ShooterSubsystem shooterSubsystem) {
-    super(driveSubsystem, intakeSubsystem, shooterSubsystem);
-    AutoTrajectory FiveBallAuto_1 = new AutoTrajectory(driveSubsystem, "FiveBallAuto_1", 3.0, 1.5);
-    AutoTrajectory FiveBallAuto_2 = new AutoTrajectory(driveSubsystem, "FiveBallAuto_2", 3.0, 1.5);
-
+    AutoTrajectory FiveBallAuto_1 = new AutoTrajectory(driveSubsystem, "ThreeBallAuto_1", 3.0, 1.5);
+    AutoTrajectory FiveBallAuto_2 = new AutoTrajectory(driveSubsystem, "ThreeBallAuto_2", 3.0, 1.5);
+    AutoTrajectory FiveBallAuto_3 = new AutoTrajectory(driveSubsystem, "FiveBallAuto_3", 3.0, 1.5);
+    AutoTrajectory FiveBallAuto_4 = new AutoTrajectory(driveSubsystem, "ThreeBallAuto_3", 3.0, 1.5);
+    
     addCommands(
-      // Get two balls from the terminal
+      // Leaves tarmac, gets a ball, and returns
       FiveBallAuto_1.getCommandAndStop().deadlineWith(new IntakeCommand(intakeSubsystem, shooterSubsystem)),
+
+      // Shoot both stored ball and new ball
+      new ShootCommand(shooterSubsystem, Constants.SHOOT_DELAY, SelectedGoal.Low),
+
+      // Leave tarmac again and get one more ball, then return
+      FiveBallAuto_2.getCommandAndStop().deadlineWith(new IntakeCommand(intakeSubsystem, shooterSubsystem)),
+
+      // Shoot ball
+      new ShootCommand(shooterSubsystem, Constants.SHOOT_DELAY, SelectedGoal.Low),
+
+      // Get two balls from the terminal
+      FiveBallAuto_3.getCommandAndStop().deadlineWith(new IntakeCommand(intakeSubsystem, shooterSubsystem)),
 
       // Shoot balls
       new ShootCommand(shooterSubsystem, Constants.SHOOT_DELAY, SelectedGoal.Low),
 
       // Leave tarmac
-      FiveBallAuto_2.getCommandAndStop(),
+      FiveBallAuto_4.getCommandAndStop(),
 
       // Reset drive PID and reverse motors again
       new InstantCommand(() -> driveSubsystem.teleopInit(), driveSubsystem)
