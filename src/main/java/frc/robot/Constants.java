c82e544d347f2011a42aa750f43ee5af6be58ab3
Vignesh Balasubramaniam <vbalasubrama@lenovo.com>
2022-04-18 13:53:38 -0400

    Minor fixes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -54,15 +54,15 @@ public final class Constants {
   // Drive PID values
   public static final double DRIVE_kP = 0.02;
   public static final double DRIVE_kD = 0.00042;
-  public static final double DRIVE_TURN_SCALAR = 20.0;
-  public static final double DRIVE_LOOKAHEAD = 15;
+  public static final double DRIVE_TURN_SCALAR = 15.0;
+  public static final double DRIVE_LOOKAHEAD = 16;
 
   private static final double DRIVE_THROTTLE_INPUT_CURVE_X[] = { 0.0, 0.5,   1.0 };
   private static final double DRIVE_THROTTLE_INPUT_CURVE_Y[] = { 0.0, 1.883, 3.766 };
   private static final double DRIVE_TRACTION_CONTROL_CURVE_X[] = { 0.0, 1.883, 3.766 };
   private static final double DRIVE_TRACTION_CONTROL_CURVE_Y[] = { 0.0, 0.5,   1.0 };
   private static final double DRIVE_TURN_INPUT_CURVE_X[] = { 0.0, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 1.0 };
-  private static final double DRIVE_TURN_INPUT_CURVE_Y[] = { 0.0, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 1.0 };
+  private static final double DRIVE_TURN_INPUT_CURVE_Y[] = { 0.0, 0.01, 0.04, 0.09, 0.16, 0.25, 0.36, 0.49, 0.64, 0.81, 1.0 };
 
   public static final PolynomialSplineFunction DRIVE_THROTTLE_INPUT_CURVE = SPLINE_INTERPOLATOR.interpolate(DRIVE_THROTTLE_INPUT_CURVE_X, DRIVE_THROTTLE_INPUT_CURVE_Y);
   public static final PolynomialSplineFunction DRIVE_TRACTION_CONTROL_CURVE = SPLINE_INTERPOLATOR.interpolate(DRIVE_TRACTION_CONTROL_CURVE_X, DRIVE_TRACTION_CONTROL_CURVE_Y);
@@ -164,7 +164,7 @@ public final class Constants {
   public static final FlywheelSpeed FLYWHEEL_IDLE_SPEED = new FlywheelSpeed(500.0, 500.0);
   public static final boolean FLYWHEEL_IDLE_DEFAULT_ENABLED = false;
   public static final List<Entry<Double, FlywheelSpeed>> FLYWHEEL_VISION_MAP = Arrays.asList(
-    Map.entry(0.00, new FlywheelSpeed(2700.0, 700.0)),
+    Map.entry(0.00, new FlywheelSpeed(2300.0, 900.0)),
     Map.entry(1.10, new FlywheelSpeed(2300.0, 900.0)),
     Map.entry(1.20, new FlywheelSpeed(2300.0, 900.0)),
     Map.entry(1.30, new FlywheelSpeed(2350.0, 940.0)),
