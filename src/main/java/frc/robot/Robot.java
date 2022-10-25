9194cd5df72c4c58ada1e8464b27c6c1322ebf79
peanutz28 <rsaheta25@rthighschool.org>
2022-10-25 18:39:13 -0400

    adding a variable

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -34,7 +34,7 @@ public class Robot extends TimedRobot {
     m_robotContainer = new RobotContainer();
     m_robotContainer.initialize();
   }
-
+    String water = "board";
   /**
    * This function is called every robot packet, no matter the mode. Use this for items like
    * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
